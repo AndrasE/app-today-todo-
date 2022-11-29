839465b784ef22ee8554232daa25df5f01ea9dbb
Andras E <rohadtsajt@gmail.com>
2022-11-29 12:15:12 +0000

    ico

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require('path')
 const favicon = require('serve-favicon')
 var _ = require("lodash");
 
@@ -10,8 +11,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(express.static(__dirname + '/public'));
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 mongoose.connect("mongodb+srv://andras:" + process.env.MONGOOSE_PASS + "@cluster0.zfr0d.mongodb.net/todolistDB", {
   useNewUrlParser: true,
